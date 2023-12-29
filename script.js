90332a05cda4948d71f7b906bbaf12a9a294b69f
Rafiw Idrissu <dev.rafiu@gmail.com>
2023-12-29 11:22:52 -0800

    feat: implement filter by region

--- script.js
+++ script.js
@@ -1,10 +1,71 @@
 let darkMode = localStorage.getItem("darkmode");
 
-const countriesGrid = document.getElementById("countries-grid");
-const countryTemplate = document.getElementById("country-template");
 const darkmodeToggle = document.querySelector("#dark-mode-toggle");
 const lightModeIcon = document.querySelector("#lightmode-icon");
 const darkModeIcon = document.querySelector("#darkmode-icon");
+
+const countriesGrid = document.getElementById("countries-grid");
+const countryTemplate = document.getElementById("country-template");
+const regionFilter = document.querySelector("#region-filter");
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+darkmodeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkmode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+
+fetchCountries(
+  "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital"
+);
+
+// countries filter by region
+regionFilter.addEventListener("change", (e) => {
+  const region = e.target.value;
+  fetchCountries(
+    `https://restcountries.com/v3.1/region/${region}?fields=name,flags,population,region,capital`
+  );
+});
+
+async function fetchCountries(api) {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    createCountries(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function createCountries(data) {
+  countriesGrid.innerHTML = ``;
+
+  data.forEach((item) => {
+    const { name, population, region, capital, flags } = item;
+    console.log(item);
+    const country = document.importNode(countryTemplate.content, true);
+    const countryFlag = country.querySelector("#country__flag");
+    const countryName = country.querySelector("#country__name");
+    const countryPopulation = country.querySelector("#population");
+    const countryRegion = country.querySelector("#region");
+    const countryCapital = country.querySelector("#capital");
+
+    countryFlag.alt = flags.alt;
+    countryFlag.src = flags.png;
+    countryName.innerText = name.common;
+    countryPopulation.innerText = population;
+    countryRegion.innerText = region;
+    countryCapital.innerText = capital;
+
+    countriesGrid.appendChild(country);
+  });
+}
 
 function enableDarkMode() {
   document.body.classList.add("darkmode");
@@ -21,53 +82,3 @@ function disableDarkMode() {
   lightModeIcon.style.display = "inline-block";
   darkModeIcon.style.display = "none";
 }
-
-if (darkMode === "enabled") {
-  enableDarkMode();
-}
-
-darkmodeToggle.addEventListener("click", () => {
-  darkMode = localStorage.getItem("darkmode");
-  if (darkMode !== "enabled") {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
-  }
-});
-
-async function fetchCountries(api) {
-  try {
-    const response = await fetch(api);
-    const data = await response.json();
-    data.forEach((item) => {
-      const { name, population, region, capital, flags } = item;
-      // console.log(item);
-      //   const name = item.name;
-      //   const population = item.population;
-      //   const region = item.region;
-      //   const capital = item.capital;
-
-      const country = document.importNode(countryTemplate.content, true);
-      const countryFlag = country.querySelector("#country__flag");
-      const countryName = country.querySelector("#country__name");
-      const countryPopulation = country.querySelector("#population");
-      const countryRegion = country.querySelector("#region");
-      const countryCapital = country.querySelector("#capital");
-
-      countryFlag.alt = flags.alt;
-      countryFlag.src = flags.png;
-      countryName.innerText = name.common;
-      countryPopulation.innerText = population;
-      countryRegion.innerText = region;
-      countryCapital.innerText = capital;
-
-      countriesGrid.appendChild(country);
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-fetchCountries(
-  "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital"
-);
